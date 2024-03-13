375fe0d35648a74eced13cfba0a8f783330ace60
NurlailaAli <161990259+NurlailaAli@users.noreply.github.com>
2024-03-13 05:55:01 -0700

    Add files via upload

---
+++ tgs3soal2.py
@@ -0,0 +1,11 @@
+# Data list pertama sebagai keys
+keys_list = ['minggu', 'bulan', 'tahun']
+
+# Data list kedua sebagai values
+values_list = ['7 hari', '4 minggu', '12 bulan']
+
+# Mengonversi dua data list menjadi dictionary
+result_dict = dict(zip(keys_list, values_list))
+
+# Menampilkan hasil
+print("Hasil Dictionary  :", result_dict)
